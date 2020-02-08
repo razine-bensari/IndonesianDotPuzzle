13b1f1fcc910c977983daf0b0e9dd20de86340e1
Razine Bensari <bensaria97@gmail.com>
2020-02-08 15:29:44 -0500

    fixin errors

--- domain/IndonesianDotPuzzle.py
+++ domain/IndonesianDotPuzzle.py
@@ -6,77 +6,77 @@ class IndonesianDotPuzzle:
     #Using adjacent list structure (dictionary of all nodes. All values of dic are list of children node related to parent node - Key)
     def __init__(self, textLine):
         self.tree = defaultdict(list)
-        textArray = textLine.split()
-        self.size = textArray[0]
-        self.max_depth = textArray[1]
+        self.textArray = textLine.split()
+        self.size = self.textArray[0]
+        self.max_depth = self.textArray[1]
         self.max_length = textArray[2]
-        puzzleArray = textwrap.wrap(textArray[3], self.size)
+        self.puzzleArray = textwrap.wrap(textArray[3], self.size)
         self.puzzle = []
 
         for row in puzzleArray:
             puzzle.append(list(str(row)))
     
-    def touch(y, x, puzzleState):
+    def touch(self, y, x, puzzleState):
 
-        temp = puzzleState
+        self.temp = puzzleState
 
         # Always switch the touched value
-        temp[y][x] = switch(temp[y][x])
+        self.temp[y][x] = self.switch(self.temp[y][x])
 
         # Handeling leftmost & rightmost columns
         if y == 0:
-            temp[y + self.size][x] = switch(temp[y + self.size][x])
+            self.temp[y + self.size][x] = self.switch(self.temp[y + self.size][x])
         
         if y == self.size - 1:
-            temp[y - self.size][x] = switch(temp[y - self.size][x])
+            self.temp[y - self.size][x] = self.switch(self.temp[y - self.size][x])
         
         if y != 0 && y != self.size - 1:
-            temp[y + self.size][x] = switch(temp[y + self.size][x])
-            temp[y - self.size][x] = switch(temp[y - self.size][x])
+            self.temp[y + self.size][x] = self.switch(self.temp[y + self.size][x])
+            self.temp[y - self.size][x] = self.switch(self.temp[y - self.size][x])
         
         if x == 0:
-            temp[y][x + 1] = switch(temp[y][x + 1])
+            self.temp[y][x + 1] = self.switch(self.temp[y][x + 1])
         
         if x == self.size - 1:
-            temp[y][x - 1] = switch(temp[y][x - 1])
+            self.temp[y][x - 1] = self.switch(self.temp[y][x - 1])
         
         if x != 0 && x != self.size - 1:
-            temp[y][x + 1] = switch(temp[y][x + 1])
-            temp[y][x - 1] = switch(temp[y][x - 1])
+            self.temp[y][x + 1] = self.switch(self.temp[y][x + 1])
+            self.temp[y][x - 1] = self.switch(self.temp[y][x - 1])
         
-        return temp
+        return self.temp
 
-    def switch(val):
+    def switch(self, val):
         return 1 - val
 
-    def generateTreeAsAdjacencyList():
+    def generateTreeAsAdjacencyList(self):
 
-        root = Node(0, 0, 0, self.puzzle)
-        currentNode = root
+        self.root = Node(0, 0, 0, self.puzzle)
+        self.currentNode = self.root
 
-        nodeCounter = 1
-        counter = 0
+        self.nodeCounter = 1
+        self.counter = 0
 
-        numOfChildren = self.size * self.size
-        maxNodeNumber = (numOfChildren)^self.max_depth
+        self.numOfChildren = self.size * self.size
+        self.maxNodeNumber = (self.numOfChildren)^self.max_depth
 
 
-        while nodeCounter < maxNodeNumber:
+        while self.nodeCounter < self.maxNodeNumber:
             #childrenListSpace, nodeCounter = generateChildrenFromNode(currentNode, nodeCounter)
-            for i in range(numOfChildren):
-                tempChildList, nodeCounter = generateChildrenFromNode(currentNode, nodeCounter)
-                self.tree[counter].extend(tempChildList)
-                currentNode = self.tree[counter//numOfChildren][i]
-                counter++
+            for i in range(self.numOfChildren):
+                self.tempChildList, self.nodeCounter = self.generateChildrenFromNode(self, self.currentNode, self.nodeCounter)
+                self.tree[self.counter].extend(self.tempChildList)
+                self.currentNode = self.tree[self.counter//self.numOfChildren][i]
+                self.counter++
 
-    def generateChildrenFromNode(parentNode, nodeCounter):
-        childrenList = []
+    def generateChildrenFromNode(self, parentNode, nodeCounter):
+        self.childrenList = []
         for i in range(self.size):
                 for j in range(self.size):
-                    childrenList.append(Node(0, 0, 0, touch(y, x, parentNode.puzzleState)))
-                    nodeCounter++
-        return childrenList, nodeCounter
-            
+                    self.childrenList.append(Node(0, 0, 0, touch(y, x, parentNode.puzzleState)))
+                    self.nodeCounter++
+        return self.childrenList, self.nodeCounter
+
 
 
 #Node class used for the graph data structure
--- main.py
+++ main.py
@@ -2,4 +2,15 @@ from domain.IndonesianDotPuzzle import *
 from StateSpaceSearchAlgorithm.DFS import *
 
 
-initialPuzzleState = IndonesianDotPuzzle("4 9 50 1110100111000111")
+puzzle = IndonesianDotPuzzle("3 5 50 110100110")
+
+puzzle.generateTreeAsAdjacencyList()
+
+fileName = "solutiontesting.txt"
+
+
+f = open(fileName, "w")
+
+
+
+f.close()
