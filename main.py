624d0b7cb61c738e860840e2c60bf0095d419657
Razine Bensari <bensaria97@gmail.com>
2020-02-28 14:26:38 -0500

    annes

--- main.py
+++ main.py
@@ -66,7 +66,12 @@ from StateSpaceSearchAlgorithm.DFS import *
 # puzzle = IndonesianDotPuzzle("3 10 10000 111111111")
 # puzzle.BFS()
 
-puzzle = IndonesianDotPuzzle("3 10 1000 111111111")
+puzzle = IndonesianDotPuzzle("3 100 2000 011001100")
+puzzle.A_star()
+
+puzzle = IndonesianDotPuzzle("3 4 2000 110110111")
 puzzle.A_star()
 
 
+puzzle = IndonesianDotPuzzle("10 2 20000 0000000000000000000000000000000000000000000100000000111000000001000000000000000000000000000000000000")
+puzzle.A_star()
